d8b2b3957e66102653c8c142b4e834302108d733
Agus Velazquez <30300165+agvelazquez@users.noreply.github.com>
2023-03-18 10:14:19 -0300

    Update ddl.sql

--- parte-1/ddl.sql
+++ parte-1/ddl.sql
@@ -134,7 +134,7 @@ CREATE TABLE stg.order_line_sale
                               orden      VARCHAR(10)
                             , producto   VARCHAR(10)
                             , tienda     SMALLINT
-                            , fecha      VARCHAR(10)
+                            , fecha      date
                             , cantidad   int
                             , venta      decimal(18,5)
                             , descuento  decimal(18,5)

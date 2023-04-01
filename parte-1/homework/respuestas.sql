5d40fe9d482962cda46e8f7d8e31a54956af1caa
DataEngIII2023 <126905289+DataEngIII2023@users.noreply.github.com>
2023-04-01 16:58:46 -0300

    3 and 4

--- parte-1/homework/respuestas.sql
+++ parte-1/homework/respuestas.sql
@@ -5,3 +5,18 @@ where categoria = 'Electro'
 --2
 select * from stg.product_master
 where origen = 'China'
+
+--3
+select * 
+from stg.product_master
+
+where categoria = 'Electro'
+order by nombre
+
+--4
+select * 
+from stg.product_master
+
+where subcategoria = 'TV' and is_active = true
+
+--5

9014a5796394b96897bfcacd2844a296f0287f97
DataEngIII2023 <126905289+DataEngIII2023@users.noreply.github.com>
2023-04-01 17:56:47 -0300

    Full homework

--- parte-1/homework/respuestas.sql
+++ parte-1/homework/respuestas.sql
@@ -20,3 +20,53 @@ from stg.product_master
 where subcategoria = 'TV' and is_active = true
 
 --5
+select * 
+from stg.store_master
+
+where pais = 'Argentina'
+order by fecha_apertura
+
+--6
+select * 
+from stg.order_line_sale
+
+order by fecha desc
+limit 5
+
+--7
+select * 
+from stg.super_store_count
+
+order by fecha 
+limit 10
+
+--8
+select * 
+from stg.product_master
+
+where categoria = 'Electro' and not subsubcategoria in ('Soporte','Control remoto')
+
+--9
+select * 
+from stg.order_line_sale
+
+where moneda in ('ARS','URU') -- tomando Pesos como Pesos Argentinos y Pesos Uruguayos
+and venta > 100000
+
+--10
+select * 
+from stg.order_line_sale
+
+where fecha between '2022-10-01' and '2022-10-31'
+
+--11
+select * 
+from stg.product_master
+
+where ean is not null
+
+--12
+select * 
+from stg.order_line_sale
+
+where fecha between '2022-10-01' and '2022-11-10'

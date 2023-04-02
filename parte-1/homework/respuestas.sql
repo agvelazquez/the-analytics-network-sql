289b2b68291cba5acd3c8d1a8981e486a3bb957b
DataEngIII2023 <126905289+DataEngIII2023@users.noreply.github.com>
2023-04-02 11:01:36 -0300

    Update respuestas.sql

--- parte-1/homework/respuestas.sql
+++ parte-1/homework/respuestas.sql
@@ -1,3 +1,4 @@
+--CLASE 1:
 --1
 select * from stg.product_master
 where categoria = 'Electro'
@@ -70,3 +71,51 @@ select *
 from stg.order_line_sale
 
 where fecha between '2022-10-01' and '2022-11-10'
+
+
+CLASE 2:
+--1 Cuales son los paises donde la empresa tiene tiendas?
+select distinct pais
+from stg.store_master
+
+--2 Cuantos productos por subcategoria tiene disponible para la venta?
+
+
+--3 Cuales son las ordenes de venta de Argentina de mayor a $100.000?
+
+
+--4 Obtener los decuentos otorgados durante Noviembre de 2022 en cada una de las monedas?
+
+
+--5 Obtener los impuestos pagados en Europa durante el 2022.
+
+
+--6 En cuantas ordenes se utilizaron creditos?
+
+
+--7 Cual es el % de descuentos otorgados (sobre las ventas) por tienda?
+
+
+--8 Cual es el inventario promedio por dia que tiene cada tienda?
+
+
+--9 Obtener las ventas netas y el porcentaje de descuento otorgado por producto en Argentina.
+
+
+--10 Las tablas "market_count" y "super_store_count" representan dos sistemas distintos que usa la empresa para contar la cantidad de gente que ingresa a tienda, uno para las tiendas de Latinoamerica y otro para Europa. Obtener en una unica tabla, las entradas a tienda de ambos sistemas.
+
+
+--11 Cuales son los productos disponibles para la venta (activos) de la marca Phillips?
+
+
+--12 Obtener el monto vendido por tienda y moneda y ordenarlo de mayor a menor por valor nominal.
+
+
+--13 Cual es el precio promedio de venta de cada producto en las distintas monedas? Recorda que los valores de venta, impuesto, descuentos y creditos es por el total de la linea.
+
+
+--14 Cual es la tasa de impuestos que se pago por cada orden de venta?
+
+
+
+CLASE 3:

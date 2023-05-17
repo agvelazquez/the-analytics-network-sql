9d2e97ecc8faff0c96c46606f1b25a9fbc2035ce
Agus Velazquez <30300165+agvelazquez@users.noreply.github.com>
2023-05-17 19:50:39 -0300

    Update dim_cost.sql

--- parte-3/project/retail-data-warehouse/dim/dim_cost.sql
+++ parte-3/project/retail-data-warehouse/dim/dim_cost.sql
@@ -5,5 +5,8 @@ DROP TABLE IF EXISTS dim.cost;
 CREATE TABLE IF NOT EXISTS dim.cost
 (
     product_id varchar(10) PRIMARY KEY,
-    cost_usd numeric
+    cost_usd numeric,
+	constraint fk_product_id_cost
+		foreign key (product_id)
+		references dim.product_master(codigo_producto)
 )

f6d54a4688eeecdae91203a068ecc945c622cab9
Agus Velazquez <30300165+agvelazquez@users.noreply.github.com>
2023-08-06 18:45:46 -0300

    Update and rename proyecto-respuestas.sql to project-answers.sql

---
+++ parte-1/project/project-answers.sql
@@ -0,0 +1,30 @@
+
+-- General 
+-- - Ventas brutas, netas y margen
+
+-- - Margen por categoria de producto 
+
+-- - ROI por categoria de producto. ROI = ventas netas / Valor promedio de inventario 
+
+-- - AOV (Average order value), valor promedio de la orden. 
+
+-- Contabilidad
+-- - Impuestos pagados
+
+-- - Tasa de impuesto. Impuestos / Ventas netas 
+
+-- - Cantidad de creditos otorgados
+
+-- - Valor pagado final por order de linea. Valor pagado: Venta - descuento + impuesto - credito
+
+-- Supply Chain
+-- - Costo de inventario promedio por tienda
+
+-- - Costo del stock de productos que no se vendieron por tienda
+
+-- - Cantidad y costo de devoluciones
+
+
+-- Tiendas
+-- - Ratio de conversion. Cantidad de ordenes generadas / Cantidad de gente que entra
+

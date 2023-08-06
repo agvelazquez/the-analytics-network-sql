5e809c92cc4067f556f0d2eb9721ae906db17ba0
Agus Velazquez <30300165+agvelazquez@users.noreply.github.com>
2023-08-06 18:10:16 -0300

    Update respuestas.sql

--- parte-1/homework/respuestas.sql
+++ parte-1/homework/respuestas.sql
@@ -100,10 +100,10 @@ select * from stg.product_master where categoria = 'Electro'
 -- 7. Crear una tabla llamada "employees" (por el momento vacia) que tenga un id (creado de forma incremental), nombre, apellido, fecha de entrada, fecha salida, telefono, pais, provincia, codigo_tienda, posicion. Decidir cual es el tipo de dato mas acorde.
   
 -- 8. Insertar nuevos valores a la tabla "employees" para los siguientes 4 empleados:
-    - Juan Perez, 2022-01-01, telefono +541113869867, Argentina, Santa Fe, tienda 2, Vendedor.
-    - Catalina Garcia, 2022-03-01, Argentina, Buenos Aires, tienda 2, Representante Comercial
-    - Ana Valdez, desde 2020-02-21 hasta 2022-03-01, España, Madrid, tienda 8, Jefe Logistica
-    - Fernando Moralez, 2022-04-04, España, Valencia, tienda 9, Vendedor.
+    -- Juan Perez, 2022-01-01, telefono +541113869867, Argentina, Santa Fe, tienda 2, Vendedor.
+    -- Catalina Garcia, 2022-03-01, Argentina, Buenos Aires, tienda 2, Representante Comercial
+    -- Ana Valdez, desde 2020-02-21 hasta 2022-03-01, España, Madrid, tienda 8, Jefe Logistica
+    -- Fernando Moralez, 2022-04-04, España, Valencia, tienda 9, Vendedor.
 
   
 -- 9. Crear un backup de la tabla "cost" agregandole una columna que se llame "last_updated_ts" que sea el momento exacto en el cual estemos realizando el backup en formato datetime.

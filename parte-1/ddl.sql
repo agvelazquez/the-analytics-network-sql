73d4b468ac24ad3f404b66a6cecfd45673f0d46d
Agus Velazquez <30300165+agvelazquez@users.noreply.github.com>
2023-08-12 18:15:31 -0300

    Update ddl.sql

--- parte-1/ddl.sql
+++ parte-1/ddl.sql
@@ -12,18 +12,18 @@ DROP TABLE IF EXISTS stg.product_master ;
     
 CREATE TABLE stg.product_master
                  (
-                              codigo_producto VARCHAR(255)
-                            , nombre          VARCHAR(255)
-                            , categoria       VARCHAR(255)
-                            , subcategoria    VARCHAR(255)
-                            , subsubcategoria VARCHAR(255)
+                              product_code    VARCHAR(255)
+                            , name            VARCHAR(255)
+                            , category        VARCHAR(255)
+                            , subcategory     VARCHAR(255)
+                            , subsubcategory  VARCHAR(255)
                             , material        VARCHAR(255)
                             , color           VARCHAR(255)
-                            , origen          VARCHAR(255)
+                            , origin          VARCHAR(255)
                             , ean             bigint
                             , is_active       boolean
                             , has_bluetooth   boolean
-                            , talle           VARCHAR(255)
+                            , size            VARCHAR(255)
                  );
     
 /* Crea tabla cost
@@ -33,8 +33,8 @@ DROP TABLE IF EXISTS stg.cost;
     
 CREATE TABLE stg.cost
                  (
-                              codigo_producto    VARCHAR(10)
-                            , costo_promedio_usd DECIMAL
+                              product_code    VARCHAR(10)
+                            , product_cost_usd DECIMAL
                  );
     
 DROP TABLE IF EXISTS stg.inventory;
@@ -44,10 +44,10 @@ Conteo de inventario al inicio y final del dia por fecha, tienda y codigo
 */
 CREATE TABLE stg.inventory
                  (
-                              tienda  SMALLINT
-                            , sku     VARCHAR(10)
-                            , fecha   DATE
-                            , inicial SMALLINT
+					 		  date   DATE
+                            , store_id  SMALLINT
+                            , item_id     VARCHAR(10)
+                            , initial SMALLINT
                             , final   SMALLINT
                  );
     
@@ -58,9 +58,9 @@ DROP TABLE IF EXISTS stg.market_count;
     
 CREATE TABLE stg.market_count
                  (
-                              tienda SMALLINT
-                            , fecha  INTEGER
-                            , conteo SMALLINT
+                              store_id SMALLINT
+                            , date  INTEGER
+                            , traffic SMALLINT
                  );
     
 /* Crea tabla monthly_average_fx_rate
@@ -70,10 +70,10 @@ DROP TABLE IF EXISTS stg.monthly_average_fx_rate;
     
 CREATE TABLE stg.monthly_average_fx_rate
                  (
-                              mes                 DATE
-                            , cotizacion_usd_peso DECIMAL
-                            , cotizacion_usd_eur DECIMAL
-                            , cotizacion_usd_uru  DECIMAL
+                              month        DATE
+                            , fx_rate_usd_peso DECIMAL
+                            , fx_rate_usd_eur DECIMAL
+                            , fx_rate_usd_uru  DECIMAL
                  );
     
 
@@ -84,16 +84,16 @@ DROP TABLE IF EXISTS stg.store_master;
       
 CREATE TABLE stg.store_master
                  (
-                              codigo_tienda  SMALLINT
-                            , pais           VARCHAR(100)
-                            , provincia      VARCHAR(100)
-                            , ciudad         VARCHAR(100)
-                            , direccion      VARCHAR(255)
-                            , nombre         VARCHAR(255)
-                            , tipo           VARCHAR(100)
-                            , fecha_apertura DATE
-                            , latitud        DECIMAL(10, 8)
-                            , longitud       DECIMAL(11, 8)
+                              store_id  SMALLINT
+                            , country           VARCHAR(100)
+                            , province      VARCHAR(100)
+                            , city         VARCHAR(100)
+                            , address      VARCHAR(255)
+                            , name         VARCHAR(255)
+                            , type           VARCHAR(100)
+                            , start_date DATE
+                            , latitude     DECIMAL(10, 8)
+                            , longitude     DECIMAL(11, 8)
                  );
     
 /* Crea tabla super_store_count
@@ -103,9 +103,9 @@ DROP TABLE IF EXISTS stg.super_store_count;
     
 CREATE TABLE stg.super_store_count
                  (
-                              tienda SMALLINT
-                            , fecha  VARCHAR(10)
-                            , conteo SMALLINT
+                              store_id SMALLINT
+                            , date  VARCHAR(10)
+                            , traffic SMALLINT
                  );
     
 /* Crea tabla order_sales_units
@@ -115,16 +115,16 @@ DROP TABLE IF EXISTS stg.order_line_sale;
     
 CREATE TABLE stg.order_line_sale
                  (
-                              orden      VARCHAR(10)
-                            , producto   VARCHAR(10)
-                            , tienda     SMALLINT
-                            , fecha      date
-                            , cantidad   int
-                            , venta      decimal(18,5)
-                            , descuento  decimal(18,5)
-                            , impuestos  decimal(18,5)
-                            , creditos   decimal(18,5)
-                            , moneda     varchar(3)
+                              order_number      VARCHAR(10)
+                            , product   VARCHAR(10)
+                            , store     SMALLINT
+                            , date      date
+                            , quantity   int
+                            , sale      decimal(18,5)
+                            , promotion  decimal(18,5)
+                            , tax  decimal(18,5)
+                            , credit   decimal(18,5)
+                            , currency     varchar(3)
                             , pos        SMALLINT
                             , is_walkout BOOLEAN
                  );

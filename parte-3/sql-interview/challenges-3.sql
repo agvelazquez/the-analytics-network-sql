e2fb3cbe8a3b774b63ad92ace8d321d84af09a3d
Agus Velazquez <30300165+agvelazquez@users.noreply.github.com>
2023-09-03 11:19:05 -0300

    Update challenges-3.sql

--- parte-3/sql-interview/challenges-3.sql
+++ parte-3/sql-interview/challenges-3.sql
@@ -3,6 +3,8 @@ Desafio Entrevista Tecnica Parte 3
 */
 
 --Ejercicio 1
+drop table if exists test.emp_2022;
+drop table if exists test.emp_2023;
 create table test.emp_2022
 ( 	emp_id int,
 	designation varchar(20));
@@ -11,16 +13,16 @@ create table test.emp_2023
 ( 	emp_id int,
 	designation varchar(20));
 
-insert into test.emp_2022 values (1,'Trainee'), (2,'Developer'),(3,'Senior Developer'),(4,'Manager');
-insert into test.emp_2023 values (1,'Developer'), (2,'Developer'),(3,'Manager'),(5,'Trainee');
+insert into test.emp_2022 values (1, 'Trainee'), (2, 'Developer'),(3, 'Senior Developer'),(4,'Manager');
+insert into test.emp_2023 values (1, 'Developer'), (2, 'Developer'),(3, 'Manager'),(5,'Trainee');
 
 /*
-Armar una tabla con el id del empleado y una columna que represente si el empleado "Ascendio" , "Renuncio", o se "Incorporo". 
-En caso de no haber cambios, no mostrarlo. Un empleado renuncia cuando esta el primer año y no el segundo, y
-viceversa para cuando un empleado se incorpora.
+- Armar una tabla con el id del empleado y una columna que represente si el empleado "Ascendio" , "Renuncio", o se "Incorporo". En caso de no haber cambios, no mostrarlo.
+    - Un empleado renuncia cuando esta el primer año y no el segundo, y viceversa para cuando un empleado se incorpora.
 */
 
 --Ejercicio 2
+drop table if exists test.orders;
 create table test.orders (
 	order_id integer,
 	customer_id integer,
@@ -41,12 +43,12 @@ insert into test.orders values
 ;
 
 /*
-Encontrar para cada dia, cuantas ordenes fueron hechas por clientes nuevos ("first_purchase") y 
-cuantas fueron hechas por clientes que ya habian comprado ("repeat_customer"). 
-Este es un concepto que se utiliza mucho en cualquier empresa para entender la capacidad de generar clientes nuevos o de retener los existentes.
+Encontrar para cada dia, cuantas ordenes fueron hechas por clientes nuevos ("first_purchase") y cuantas fueron hechas por clientes que ya habian comprado ("repeat_customer"). Este es un concepto que se utiliza mucho en cualquier empresa para entender la capacidad de generar clientes nuevos o de retener los existentes.
 */
 
 --Ejercicio 3
+drop table if exists test.orders2;
+drop table if exists test.products;
 create table test.orders2(
 	order_id int,
 	customer_id int,
@@ -71,9 +73,7 @@ insert into test.products VALUES
 (3, 'C'),
 (4, 'D'),
 (5, 'E');
-
 /*
-Armar una tabla que sirva como una version simplificada de un sistema de recomendacion y muestre, 
-cuantas ordenes se llevan por cada PAR de productos.
+Armar una tabla que sirva como una version simplificada de un sistema de recomendacion y muestre, cuantas ordenes se llevan por cada PAR de productos.
 */
 

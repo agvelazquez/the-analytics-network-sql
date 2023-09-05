a4e65af6dedfc50473225b5ecbe9a4db02528195
Agus Velazquez <30300165+agvelazquez@users.noreply.github.com>
2023-09-05 17:05:03 -0300

    Update ddl.sql

--- parte-1/ddl.sql
+++ parte-1/ddl.sql
@@ -126,3 +126,19 @@ CREATE TABLE stg.order_line_sale
                             , pos        SMALLINT
                             , is_walkout BOOLEAN
                  );
+
+-- Table: stg.return_movements
+DROP TABLE IF EXISTS stg.return_movements;
+
+CREATE TABLE IF NOT EXISTS stg.return_movements
+(
+    order_id varchar(255),
+    return_id varchar(255),
+    item varchar(255),
+    quantity integer,
+    movement_id integer,
+    from_location varchar(255),
+    to_location varchar(255),
+    received_by varchar(255),
+    date date
+);
